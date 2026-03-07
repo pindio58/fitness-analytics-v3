bc33c008c1d159d3f523ba8ac3caa1fbe15e36c3
Bhupinderjit Singh <pindio13@gmail.com>
2026-03-07 15:59:16 +0530

    added first DAG for straba module

---
+++ airflow/dags/dag4-strava-processing.py
@@ -0,0 +1,40 @@
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import dag, task
+
+
+from pathlib import Path
+import os
+from datetime import datetime
+
+BASE_DIR = Path(os.environ['AIRFLOW_HOME'])
+file_path = BASE_DIR / "dags"
+
+
+default_args={'owner':'pindio58',
+              'depends_on_past':False,
+              'retries':0,
+              'email_on_failure':False}
+
+
+
+@dag(default_args=default_args,
+     catchup=False,
+     dag_display_name='fitness-analytics',
+      dag_id='fitness-analytics',
+      schedule=None,
+      start_date=datetime(2025,12,1),
+      is_paused_upon_creation=False)
+def etl():
+    start = EmptyOperator(task_id='start')
+
+    refresh_token = BashOperator(
+        task_id='refresh_token',
+        bash_command='python strava_module/auth.py'
+    )
+
+    end = EmptyOperator(task_id='end')
+
+    start >> refresh_token >> end
+
+etl()
