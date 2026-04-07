bf3259edf9656d8b8e2760bb59edf8110a75514b
Bhupinderjit Singh <pindio13@gmail.com>
2026-04-07 14:33:07 +0530

    added airflow posthres port forward

--- deploy-all.sh
+++ deploy-all.sh
@@ -50,6 +50,8 @@ nohup kubectl port-forward svc/fitness-analytics-minio-service 9001:9001 -n fitn
 nohup kubectl port-forward svc/fitness-analytics-postgres-service 5433:5432 -n fitness-analytics-namespace > "$WORKDIR/postgres-port.log" 2>&1 &
 # Airflow webserver (default 8080)
 nohup kubectl port-forward svc/airflow-api-server 8080:8080 -n fitness-analytics-namespace > "$WORKDIR/airflow-port.log" 2>&1 &
+# Airflow postgres 
+nohup kubectl port-forward svc/airflow-postgresql  5432:5432 -n fitness-analytics-namespace > "$WORKDIR/airflow-port.log" 2>&1 &
 # Metabase webserver (default 3000)
 nohup kubectl port-forward svc/fitness-analytics-metabase-service 3000:3000 -n fitness-analytics-namespace > "$WORKDIR/metabase-port.log" 2>&1 &
 # Spark UI (default 4040)
